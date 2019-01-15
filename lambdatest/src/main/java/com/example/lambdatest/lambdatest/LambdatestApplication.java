8867948f6c950a212fddec3c6ac881e189ebe6a1
lindada <17512580364@163.com>
2019-01-15 14:16:34 +0800

    添加lambda测试 填写mybatis-plus

---
+++ lambdatest/src/main/java/com/example/lambdatest/lambdatest/LambdatestApplication.java
@@ -0,0 +1,14 @@
+package com.example.lambdatest.lambdatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdatestApplication.class, args);
+	}
+
+}
+
