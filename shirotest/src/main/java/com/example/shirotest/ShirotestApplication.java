e403750f129b97ba9eee6d77df7b56033a6b78e7
lindada <17512580364@163.com>
2019-02-13 10:07:23 +0800

    莫名更新

--- shirotest/src/main/java/com/example/shirotest/ShirotestApplication.java
+++ shirotest/src/main/java/com/example/shirotest/ShirotestApplication.java
@@ -1,4 +1,4 @@
-package com.example.shirotest.shirotest;
+package com.example.shirotest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
