374ab24fa1aaf358569654ee082b63ef820b3c69
lindada <17512580364@163.com>
2019-04-12 10:13:50 +0800

    删除springsecurity

--- springsecurity/oauth-server/src/main/java/com/example/oauthserver/service/SysRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.example.oauthserver.service;
-
-import com.example.oauthcommon.domain.SysRole;
-
-import java.util.Set;
-
-public interface SysRoleService {
-    Set<SysRole> findRolesByUserId(Long id);
-}
