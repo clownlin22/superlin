1db22e53b7f1623b916d38fad893157c92d60a65
lindada <17512580364@163.com>
2019-06-10 15:47:08 +0800

    easyexcel

---
+++ easyexcel/src/main/java/easyexcel/EasyExcelApplication.java
@@ -0,0 +1,15 @@
+package easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: lxy
+ * @Date: 2019/6/10 15:08
+ */
+@SpringBootApplication
+public class EasyExcelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyExcelApplication.class, args);
+    }
+}
